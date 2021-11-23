30eda9925b0aee4dc6deda80d8a0e961b89f5d81
samylox <78290626+samylox@users.noreply.github.com>
2021-11-22 18:32:13 -0700

    connecterd

--- Connetctions.py
+++ Connetctions.py
@@ -1,2 +1,10 @@
+import neo4j
+from neo4j import GraphDatabase,basic_auth
+
 uri = "neo4j://localhost:7687"
 driver = GraphDatabase.driver(uri, auth=("neo4j", "Passw0rd"))
+session = driver.session()
+q1 = 'MATCH (n) RETURN n'
+nodes = session.run(q1)
+for node in nodes:
+    print (node)
